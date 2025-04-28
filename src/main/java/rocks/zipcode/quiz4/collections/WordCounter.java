be131ad57fe67c3ad3f156a32df82c3fd1dc0023
Nathan <natefintak@gmail.com>
2025-04-28 15:30:51 -0400

    in progress

--- src/main/java/rocks/zipcode/quiz4/collections/WordCounter.java
+++ src/main/java/rocks/zipcode/quiz4/collections/WordCounter.java
@@ -1,12 +1,25 @@
 package rocks.zipcode.quiz4.collections;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class WordCounter {
+
+    String[] strings;
+
     public WordCounter(String... strings) {
+        this.strings = strings;
     }
 
     public Map<String, Integer> getWordCountMap() {
-        return null;
+        Map wordMap = new HashMap<String, Integer>();
+        for (String word : this.strings) {
+            if (!wordMap.containsKey(word)) {
+                wordMap.put(word, 1);
+            } else {
+                wordMap.put(word, (Integer)wordMap.get(word) + 1);
+            }
+        }
+        return wordMap;
     }
 }
--- src/main/java/rocks/zipcode/quiz4/collections/culonary/Food.java
+++ src/main/java/rocks/zipcode/quiz4/collections/culonary/Food.java
@@ -1,5 +1,7 @@
 package rocks.zipcode.quiz4.collections.culonary;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -7,14 +9,32 @@ import java.util.Map;
  * @author leon on 27/12/2018.
  */
 public class Food {
+
+    List<Spice> spiceList;
+
+    public Food() {
+            this.spiceList = new ArrayList<>();
+    }
+
+    public Food(ArrayList<Spice> list) {this.spiceList = list;}
+
     public List<Spice> getAllSpices() {
-        return null;
+        return this.spiceList;
     }
 
     public <SpiceType extends Class<? extends Spice>> Map<SpiceType, Integer> getSpiceCount() {
-        return null;
+        Map<SpiceType, Integer> spiceMap = new HashMap<>();
+//        for (Spice spice : this.spiceList) {
+//            if (!spiceMap.containsKey(spice)) {
+//                spiceMap.put(spice, 1);
+//            } else {
+//                spiceMap.put(spice, spiceMap.get(spice) + 1);
+//            }
+//        }
+        return spiceMap;
     }
 
     public void applySpice(Spice spice) {
+        spiceList.add(spice);
     }
 }
